9a960e5f3736002cee459e689906b10f35830b4d
roothex200 <sabiturrahman60@gmail.com>
2023-08-27 23:24:27 +0600

    added all operation

--- Linked list/main.dart
+++ Linked list/main.dart
@@ -1,88 +1,199 @@
-//make node
 
-class Node {
-  int? value;
-  var next = null;
-  Node({required this.value});
-  @override
-  String toString() => "Node(value:$value,next:$next)";
-}
-
-//2->3->4->
-
-class LinkedList {
-  Node node;
-  LinkedList({required this.node});
-  var count = 0;
-  void GetvalueFromLinklist() {
-    print(node.toString());
-    while (true) {
-      print(node.value);
-      if (node.next != null) {
-        node = node.next;
-      } else {
-        break;
-      }
-    }
-  }
-
-  void InsertValueAtLast(value) {
-    var currentNode = node;
-    var newnode = Node(value: value);
-
-    while (true) {
-      if (currentNode.next != null) {
-        currentNode = currentNode.next;
-      } else {
-        currentNode.next = newnode;
-        break;
-      }
-    }
-  }
-
-  void InsertValueAtFirst(value) {
-    var newNode = Node(value: value);
-    newNode.next = node;
-    node = newNode;
-  }
-
-  void DeleteValueAtFirst() {
-    node = node.next;
-    print(node.toString());
-  }
-
-  void DeleteValueAtLast() {
-    var currentNode = node;
-    while (true) {
-      if (currentNode.next != null) {
-        if (currentNode.next.next == null) {
-          currentNode.next = null;
-          break;
-        } else {
-          currentNode = currentNode.next;
-        }
-      } else {
-        break;
-      }
-    }
-    print(node.toString());
-  }
-}
 
 void main(List<String> args) {
-  var head = Node(value: 2);
-  LinkedList linkList = LinkedList(node: head);
-  linkList.InsertValueAtLast(20);
-  linkList.InsertValueAtLast(30);
-  linkList.InsertValueAtLast(40);
-  linkList.InsertValueAtLast(50);
-  linkList.InsertValueAtFirst(1);
-  linkList.InsertValueAtLast(30);
-  linkList.InsertValueAtLast(40);
-  linkList.InsertValueAtLast(50);
-  linkList.DeleteValueAtFirst();
-  linkList.DeleteValueAtFirst();
-  linkList.DeleteValueAtFirst();
-  linkList.DeleteValueAtLast();
-  linkList.DeleteValueAtLast();
+  LinkedList<int> list=LinkedList<int>();
+  list.insertAtFirst(1);
+  list.insertAtLast(2);
+  list.insertAtLast(3);
+  list.insertAtLast(4);
+    list.insertAtLast(100);
+    list.reverse();
+    list.insertAtLast(200);
+    list.insertAtFirst(200000);
+    list.reverse();
+    list.insertAtLast(200);
+    list.printList();
+  
+  // list.printList();
+  // // list.removeAtLast();
+  // // list.removeAtLast();
+  // // list.printList()
+  // list.remove(1);
+  // list.printList();
+  // list.printList();
+}
+
+class Node<T>{
+  final T data;
+  var next;
+  Node({required this.data,required this.next});
+    @override
+  String toString(){
+    return "{Data:-$data and next:- $next}";
+  }
+}
+
+class LinkedList<T>{
+
+  Node<T>? _head;
+  Node<T>? _tail;
+  int count=0;
+  void insertAtFirst(T item){
+    var newnode=Node(data: item, next: null);
+    if(_head==null){
+        _head=newnode;
+        _tail=newnode;
+    }else{
+      newnode.next=_head;
+      _head=newnode;
+    }
+    count=count+1;
+  }
+
+
+  void insertAtLast(T item){
+    var newnode=Node(data: item, next: null);
+
+    if(_head==null&&_tail==null){
+      _head=newnode;
+      _tail=newnode;
+    }else{
+      _tail!.next=newnode;
+      _tail=newnode;
+    }
+    count=count+1;
+  }
+
+  void reverse(){
+    var currentnode=_head;
+    var prev=null;
+    var tmcount=0;
+    while(currentnode!=null){
+      var next=currentnode.next;
+      currentnode.next=prev;
+      if(tmcount==1){
+        _tail=prev;
+      }
+      prev=currentnode;
+      currentnode=next;
+      tmcount=tmcount+1;
+    }
+    _head=prev;
+  }
+
+  void insertData(int index,T item){
+    var newnode=Node(data: item, next: null);
+    if(index==0){
+      insertAtFirst(item);
+    }else if(index==count){
+      insertAtLast(item);
+    }else if(index>count){
+      throw ArgumentError("Index out of bounds");
+      
+    }else{
+    var currentnode=_head;
+    for(int i=0;i<index-1;i++){
+      currentnode=currentnode!.next;
+    }
+    newnode.next=currentnode!.next;
+    currentnode.next=newnode;
+    count=count+1;
+    }
+  }
+
+
+  void indexof(int item){
+    int tmcount=0;
+    var currentnode=_head;
+    while(true){
+      if(item==currentnode!.data){
+          print(tmcount);
+      }
+      if(currentnode.next==null){
+          break;
+      }else{
+        tmcount=tmcount+1;
+        currentnode=currentnode.next;
+      }
+    }
+  }
+
+  void removeAtfirst(){
+   if(_head!=null){
+     _head=_head!.next;
+   }
+   count=count-1;
+  }
+
+  void removeAtLast(){
+    var currentnode=_head;
+
+    while(true){
+      if(currentnode!.next.next==null){
+        currentnode.next=null;
+        break;
+      }else{
+        currentnode=currentnode.next;
+      }
+    }
+    count=count-1;
+    _tail=currentnode;
+  }
+
+  void remove(int index){
+    if(index==0){
+      removeAtfirst();
+    }else if(index==count-1){
+      removeAtLast();
+    }else if(index>count){
+      print("Error not found index");
+    }
+    else{
+          var currentnode=_head;
+    for(int i=0;i<index-1;i++){
+      currentnode=currentnode!.next;
+    }
+    var tmp=currentnode!.next;
+    currentnode.next=tmp.next;
+    tmp.next=null;
+    print(tmp);
+    }
+    count=count-1;
+  }
+
+  bool contain(int item){
+    var currentnode=_head;
+    bool check=false;
+    while(true){
+      if(currentnode!.next!=null){
+        if(currentnode.data==item){
+         check=true;
+         break;
+        }
+      currentnode=currentnode.next;
+      }else{
+        break;
+        
+      }
+    }
+    return check;
+  }
+
+
+  void printList(){
+    var tmp=_head;
+    if(tmp==null){
+      print("not found");
+    }else{
+    while (true){
+      print(tmp!.data);
+      if(tmp.next==null){
+        break;
+      }else{
+        tmp=tmp.next;
+      }
+    }
+    }
+  }
+
 }
