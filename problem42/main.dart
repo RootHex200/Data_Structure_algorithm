39c8f10b082914a24d0eec56887dbf0b76204ebd
RootHex200 <sabiturrahman60@gmail.com>
2023-12-25 20:07:04 +0600

    change logic

--- problem42/main.dart
+++ problem42/main.dart
@@ -1,31 +1,39 @@
-
-import 'dart:math';
-
 void main(List<String> args) {
-  print(Solution().lengthOfLongestSubstring("pwwkew"));
-  
+  print(Solution().lengthOfLongestSubstring("abba"));
 }
+class Solution {
+int lengthOfLongestSubstring(String s) {
+  if (s.isEmpty) {
+    print("Input string is empty.");
+    return 0;
+  }
 
-	class Solution {
-	  int lengthOfLongestSubstring(String s) {
-		  var l = 0;
-		  var visit = <String>{};
-		  var res = 0;
+  // Create a map to store the index of each character in the string
+  Map<String, int> charIndexMap = {};
 
-		  for (var r = 0; r < s.length; r++){
-			  while (visit.contains(s[r])){
-				  visit.remove(s[l]);
-				  l += 1;
-			  }
+  // Initialize variables to keep track of the start and end of the current substring
+  int start = 0;
+  int maxLength = 0;
+  int maxStart = 0;
 
-			  visit.add(s[r]);
-        print("res = ${res}");
-			  res = max(res, r - l + 1);
-        print("l = ${l}");
-        print("r = ${r}");
-        print("res = ${res}");
-		  }
-		  return res;
+  // Iterate through the string
+  for (int end = 0; end < s.length; end++) {
+    String currentChar = s[end];
 
-	  }
-	}
+    // If the current character is already in the substring, update the start index
+    if (charIndexMap.containsKey(currentChar) && charIndexMap[currentChar]!>=start) {
+      start = charIndexMap[currentChar]! + 1;
+    }
+
+    // Update the index of the current character
+    charIndexMap[currentChar] = end;
+
+    // Update the maximum length and starting index if the current substring is longer
+    if (maxLength < (end - start + 1)) {
+      maxLength = end - start + 1;
+      maxStart = start;
+    }
+  }
+  return "${s.substring(maxStart, maxStart + maxLength)}".length;
+}
+}
